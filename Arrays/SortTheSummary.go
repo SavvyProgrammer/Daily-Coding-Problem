55d43a2d1dca6500bd896b3b6bfe32e4787cc406
Samson Ehigiator <samson.ehigiator@physci.uniben.edu>
2020-06-23 16:47:31 +0100

    sort the summary hackerrank

---
+++ Arrays/SortTheSummary.go
@@ -0,0 +1,50 @@
+package main
+// Sort the Summary Hackerrank Golang Solution
+// Given an array of integers, create a 2-dimensional array where the first element is a distinct value from
+// the array and the second element is that value's frequency within the array.
+// Sort the resulting array descending by frequency. If multiple values have the same frequency; they should
+// be sorted ascending.
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(groupSort([]int32{3, 3, 1, 2, 1, 4, 4, 5, 5, 6, 6, 9, 7, 5}))
+}
+func groupSort(arr []int32) [][]int32 {
+	counter := make(map[int32]int32)
+	for _, elem := range arr {
+		counter[elem]++
+	}
+	result := make([][]int32, len(counter))
+	var indexOfResult int
+	for key, value := range counter {
+		sliceOfKeyValue := make([]int32, 2)
+		sliceOfKeyValue[0], sliceOfKeyValue[1] = key, value
+		indexOfResult++
+		result[len(result)-indexOfResult] = sliceOfKeyValue
+	}
+	for i := 1; i < len(result); i++ {
+		freq := result[i][1]
+		slice := result[i]
+		j := i - 1
+		for j > -1 && freq > result[j][1] {
+			result[j+1] = result[j]
+			j--
+		}
+		result[j+1] = slice
+	}
+	for i := 1; i < len(result); i++ {
+		freq := result[i][1]
+		key := result[i][0]
+		slice := result[i]
+		j := i - 1
+		for j >= 0 && result[j][0] > key && freq == result[j][1] {
+			result[j+1] = result[j]
+			j--
+		}
+		result[j+1] = slice
+	}
+	return result
+}
