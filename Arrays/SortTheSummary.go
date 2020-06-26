402bbd4de11e77b7fa09c22a564eb042e8f34a40
Samson Ehigiator <samson.ehigiator@physci.uniben.edu>
2020-06-26 09:57:36 +0100

    SortTheSummary

--- Arrays/SortTheSummary.go
+++ Arrays/SortTheSummary.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	fmt.Println(groupSort([]int32{3, 3, 1, 2, 1, 4, 4, 5, 6, 9, 7, 5}))
+	fmt.Println(groupSort([]int32{3, 3, 1, 2, 1, 4, 4, 5, 6, 9, 9, 7, 5}))
 }
 func groupSort(arr []int32) [][]int32 {
 	counter := make(map[int32]int32)
@@ -22,13 +22,13 @@ func groupSort(arr []int32) [][]int32 {
 	for key, value := range counter {
 		sliceOfKeyValue := make([]int32, 2)
 		sliceOfKeyValue[0], sliceOfKeyValue[1] = key, value
+		result[indexOfResult] = sliceOfKeyValue
 		indexOfResult++
-		result[len(result)-indexOfResult] = sliceOfKeyValue
 	}
 	for i := 1; i < len(result); i++ {
+		key := result[i][0]
 		freq := result[i][1]
 		slice := result[i]
-		key := result[i][0]
 		j := i - 1
 		for j > -1 && freq > result[j][1] {
 			result[j+1] = result[j]
